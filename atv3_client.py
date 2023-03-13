9a77ef874a46892fc18106bedabf5bfa328b3dba
Lucas Dias Maciel <33635996+lucdias@users.noreply.github.com>
2023-03-12 22:18:30 -0300

    Update atv3_client.py

--- atv3_client.py
+++ atv3_client.py
@@ -1,5 +1,5 @@
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 from time import sleep
 import threading
@@ -23,27 +23,27 @@ while True:
     mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     
-    mysock.connect(('172.22.77.205', 8081))
-    print("Digite C se gostaria de enviar uma mensagem de configuracao, Digite L para receber o log de configuracao  ou R para receber o log de registros dos carros ")
+    mysock.connect(('127.0.0.1', 8080))
+    print("Digite C (config), Digite L (log)  ou R (registros): ")
     inp = input()
     if(inp == "C"):
         config["header"] ="C"
         print("Insira o nome do usuario:")
         inp = input()
         message_usuario = inp 
-        config["body"]["usuario"] =message_usuario
+        config["body"]["usuario"] = message_usuario
         print("Tempo led verde:")
         inp = input()
         message_tmp_verde = inp 
-        config["body"]["sinal_verde"] =message_tmp_verde
+        config["body"]["sinal_verde"] = message_tmp_verde
         print("Tempo led vermelho:")
         inp = input()
         message_tmp_vermelho = inp 
-        config["body"]["sinal_vermelho"] =message_tmp_vermelho
+        config["body"]["sinal_vermelho"] = message_tmp_vermelho
         print("Tempo led amarelo:")
         inp = input()
         message_tmp_amarelo = inp 
-        config["body"]["sinal_amarelo"] =message_tmp_amarelo
+        config["body"]["sinal_amarelo"] = message_tmp_amarelo
 
         print(config)
         stringJ= json.dumps(config)
@@ -64,28 +64,16 @@ while True:
             print(objetoJ)
     elif(inp == "R"):
         config["header"] ="R"
-        pass
+        stringJ= json.dumps(config)
+        mysock.sendall(bytes(stringJ, "utf-8"))
+        data = mysock.recv(1000)
+
+        if data:
+            data_msg = str(data.decode("utf-8"))
+            objetoJ= json.loads(data_msg)
+            print(objetoJ)
     else:
         pass
     
-    
 
-
-    
-
-
-    #print("mensagem: ", message)
-    
-   # mysock.sendall(bytes(message, "utf-8"))
-    
-    #data = mysock.recv(1000)
-    
-   # if data:
-   #     data_msg = str(data.decode("utf-8"))
-    #    print(data_msg)
-
-    
-    #message = ""
-    
-
-   # mysock.close()
+    print()
